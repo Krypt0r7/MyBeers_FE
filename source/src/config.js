d29c9242e9386a46aac9474544be300cd57d6995
Victor Ronnerstedt <victor.ronnerstedt@gmail.com>
2020-03-02 11:45:16 +0100

    Config change again

--- source/src/config.js
+++ source/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  // myBeerApiUrl: "http://mybeer-api.ronnerstedt.com",
-  myBeerApiUrl: "http://localhost:51210",
+  myBeerApiUrl: "http://mybeer-api.ronnerstedt.com",
+  // myBeerApiUrl: "http://localhost:51210",
   systemetApiUrl: "https://api-extern.systembolaget.se/product/v1/product",
   systemetImageBaseUrl: "https://sb-product-media-prod.azureedge.net/productimages",
   systemetAccessKey: 'ce26456ac64b43a38dbc40dc6925177c'
